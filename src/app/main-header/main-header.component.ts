8416f1437aee28e36af78c0bc8cd69b4e353967f
Suhail Mazi <Suhail@Suhails-MacBook-Pro.local>
2019-11-23 04:34:43 -0800

    finish lab 5

---
+++ src/app/main-header/main-header.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-main-header',
+  templateUrl: './main-header.component.html',
+  styleUrls: ['./main-header.component.css']
+})
+export class MainHeaderComponent implements OnInit {
+  firstName = 'Suhail';
+  lastName = 'Mazi';
+  greeting = 'r1c2';
+
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  showGreeting() {
+    //const row2 = document.getElementById("r1c2")
+    this.greeting = 'Hello ' + this.firstName + ' ' + this.lastName + '.'
+  }
+  resetGreeting() {
+    //const row2 = document.getElementById("r1c2")
+    this.greeting = 'r1c2'
+  }
+
+
+  navigateTo(path: string) {
+    this.router.navigate([path]);
+  }
+}
